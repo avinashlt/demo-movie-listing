6df26243df76a7c65efb0e3a25e54080d0d1e4bb
Avinash <avinash.lt87@gmail.com>
2019-05-01 19:26:25 +0530

    Base set up

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <left-nav></left-nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LeftNav from './components/left-nav.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LeftNav
   }
 }
 </script>
---
+++ src/components/left-nav.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="left-nav-wrapper">
+        <input type="text">
+        <ul class="movie-list">
+            <li v-for= "movie in movies" class="movie-item">
+                <p>{{movie.name}}</p>
+                <p>{{movie.year}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'LeftNav',
+     data(){
+        return {    
+            "movies":[{
+                name : "Avengers",
+                id  : 10,
+                year : "2018"
+            },{
+                name : "King in the ring",
+                id  : 10,
+                year : "2017"
+            },{
+                name : "Survival",
+                id  : 10,
+                year : "2016"
+            },{
+                name : "Sing a song",
+                id  : 10,
+                year : "2011"
+            },{
+                name : "Millions",
+                id  : 10,
+                year : "2013"
+            },{    
+                name : "Spider man",
+                id  : 10,
+                year : "2010"
+            },
+        ]}
+    }
+
+}
+</script>
+
+
+<style scoped>
+.left-nav-wrapper {
+    background: gray;
+    width: 30%;
+    height: 100vh;
+    float: left;
+}
+.movie-list {
+    list-style-type: none;
+    padding: 0
+}
+.movie-item {
+    border: 1px solid black;
+    border-bottom: none;
+}
+    
+</style>
